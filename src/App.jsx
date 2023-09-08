6d2b59de89a2fa03de254a834c98a0905b16ee2a
pankaj upadhyay <pankajup.git01@gmail.com>
2023-09-08 19:59:47 +0530

    initial commit/Tailwind css Added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div className=' font-semibold text-4xl text-blue-900 flex justify-center h-[100vh] items-center '>
+        404 Not Yet Implemented
+      </div> 
+    </>
+  )
+}
+
+export default App
