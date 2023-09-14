0fed4996b2ca63312391ab161c71da956a86763f
pankaj upadhyay <pankajup.git01@gmail.com>
2023-09-15 00:13:24 +0530

    About Point section created

---
+++ src/Component/Cards/Alnum_card.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Alnum_card = () => {
+    return (
+        <>
+            <div className='flex w-full justify-center flex-wrap m-auto gap-y-5'>
+                <div className='flex w-[calc(25%-10px)] justify-center items-start'>
+                    <div className='flex flex-col w-[250px] justify-center items-center bg-Hero h-[200px] relative before:absolute before:content-[""]  before:h-[60px] before:w-full before:bg-white before:top-0 '>
+                        <div className="w-[100px] h-[100px] hover:scale-105 z-[99] rounded-[50%] bg-[aliceblue] p-1 overflow-hidden bg-contain">
+                            <img className='w-full h-full rounded-[50%] bg-[#a5b5b6]' width={180} height={180} loading='lazy' src="" alt="" />
+                        </div>
+                        <div className='flex justify-center items-center my-5'>
+                            sdgsdfg
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Alnum_card
--- src/Component/Home.jsx/About_point.jsx
+++ src/Component/Home.jsx/About_point.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import Alnum_card from '../Cards/Alnum_card'
 
 const About_point = () => {
     return (
-        <div className='flex w-[90%] m-auto'>
-            <div className='flex w-full justify-center items-center'>
-                <h1>
+        <div className='flex w-[90%] m-auto my-5'>
+            <div className='flex flex-col w-full justify-center items-center gap-y-6'>
+                <h1 className='text-[28px] font-semibold text-blue-500'>
                     How To Get Tech Point ?
                 </h1>
+                <div className='w-full'>
+                    <Alnum_card />
+                </div>
             </div>
         </div>
     )
